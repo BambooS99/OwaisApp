53c6ee9819cf9cfea645ea98967180ecf7bc7a23
Owais Hassan <owhassan@cisco.com>
2024-09-13 14:39:06 -0400

    removed more commented code

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,7 @@ function App() {
         <p>
           <LoginForm />{" "}
         </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login
-        </a> */}
+        {}
       </header>
     </div>
   );
